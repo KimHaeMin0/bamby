7f7092f11b847b62ee37ac5579b3c5621e0ae93c
KimHaeMin0 <kimhaemin05@gmail.com>
2025-03-13 22:05:10 +0900

    first commit

---
+++ test2-4.py
@@ -0,0 +1,10 @@
+name = '김해민'
+print('나의 이름은 :', name)
+age = 22
+print('나의 나이는 :', age)
+height = 159
+print('나의 키는', height, 'cm 입니다.')
+sum = 10 + 20
+print('10 + 20 =', sum)
+mult = 10 * 20
+print('10 * 20 =', mult)
